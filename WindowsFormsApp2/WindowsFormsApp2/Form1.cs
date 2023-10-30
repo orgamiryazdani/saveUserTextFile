eaa3ff0cf6b8cb4026bbe524036fc2d4d645ef60
Amir Yazdani <100785957+orgamiryazdani@users.noreply.github.com>
2023-10-30 12:58:56 +0330

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 Instance;
+        public Form1()
+        {
+            InitializeComponent();
+            Instance = this;
+        }
+
+         Form2 form2 = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                form2.Show();
+                form2.TopMost = true;
+                form2.TopMost = false;
+            }
+            else
+            {
+                label3.Text = "لطفا هر دو تکست باکس را پر کنید";
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textBox1Value = Form1.Instance.textBox1.Text;
+            string textBox2Value = Form1.Instance.textBox2.Text;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = Path.Combine(desktopPath, "savedData.txt");
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+             writer.WriteLine("userName : " + textBox1Value);
+             writer.WriteLine("password : " + textBox2Value);
+            }
+
+            label1.Text = "اطلاعات ذخیره شد";
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
